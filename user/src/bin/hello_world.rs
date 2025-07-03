5fbbefa3cd13514c6fadb97e860bc0f2946c332c
hdu_willsky <hdu_willsky@foxmail.com>
2025-07-04 02:25:13 +0800

    fix: 111

--- user/src/bin/hello_world.rs
+++ user/src/bin/hello_world.rs
@@ -4,16 +4,12 @@
 #[macro_use]
 extern crate user_lib;
 
-use user_lib::sys_write;
 
 #[unsafe(no_mangle)]
 fn main() -> i32 {
     // 直接使用系统调用输出简单字符串
-    let hello_msg = b"[user] Hello from user program!\n";
-    sys_write(1, hello_msg);
+    println!("[user] Hello from user program!");
 
-    let exit_msg = b"[user] Program exiting\n";
-    sys_write(1, exit_msg);
-
+    println!("[user] Program exiting");
     0
 }
